ec0f2d4302e787a34bce1fbeab608f31d76ea6bb
Stackbit Code Editor <projects@stackbit.com>
2021-05-30 07:09:00 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -28,8 +28,16 @@ export default class MyDocument extends Document {
     }
 
     render() {
-        // if you don't like Helmet but you still want to set properties on body use this
-        // const pageProps = _.get(this.props, '__NEXT_DATA__.props.pageProps');
+ const gaCode = `
+ (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.'+'js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K3QRQDP');
+ `
+ 
+ 
+
         return (
             <Html {...this.helmetHtmlAttrComponents}>
                 <Head>{this.helmetHeadComponents}</Head>
